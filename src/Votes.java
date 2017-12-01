b284945f459b11fa0f4b4eeed48557329638dfff
unknown <hall1kw@cmich.edu>
2017-12-01 01:34:52 -0500

    this commit includes a main.java which we can utilize for texting code chunks.

--- src/Votes.java
+++ src/Votes.java
@@ -1,21 +1,38 @@
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Votes{
-	//Contains all of the voter's globalIDs 
-	private ArrayList <String> voterIDs = new ArrayList <String>();
-	//Contains all of a voter's individual votes
-	private ArrayList <ArrayList<String>> indVotes = new ArrayList <ArrayList<String>>();
-	
-	public Votes(){
-		
+public class Votes extends main{
+	// Contains all of the voter's globalIDs
+	private static ArrayList<String> voterIDs = new ArrayList<String>();
+	// Contains all of a voter's individual votes
+	private static ArrayList<ArrayList<String>> indVotes = new ArrayList<ArrayList<String>>();
+
+	public static void Votes() throws IOException {
+		// Set URL of raw vote data
+		URL rawResults = new URL("http://www.acmaitp.org/test.txt");
+		// Establish Scanner for the URL file
+		Scanner dataScanner = new Scanner(rawResults.openStream()).useDelimiter("[,|\n|,\r]+");
+		// Start processing data
+		while (dataScanner.hasNextLine()) {
+			String vote = dataScanner.nextLine();
+			String[] data = vote.split("[,]");
+
+			if (!checkID(data[0])) {
+				continue;
+			} else {
+				storeVote(data);
+			}
+
+		}
+		// Print statements for testing
+		printArray();
+		printIDs();
 	}
-	
-	//check to see if we used this globalID before
-	public boolean checkID(String voterID){
+
+	// check to see if we used this globalID before
+	public static boolean checkID(String voterID) {
 		if (!voterIDs.contains(voterID)) {
 			voterIDs.add(voterID);
 			return true;
@@ -23,31 +40,36 @@ public class Votes{
 			return false;
 		}
 	}
-	
+
 	/*
-	 * creates a Voter class with the voter's GlobalID, then loads the voter's votes,
-	 * OR stores a voter's votes into indVotes.
+	 * creates a Voter class with the voter's GlobalID, then loads the voter's
+	 * votes, OR stores a voter's votes into indVotes.
 	 */
-	public void storeVote(int[] indVote) throws IOException{
-		URL rawResults = new URL("http://www.acmaitp.org/test.txt");
-		Scanner dataScanner = new Scanner(rawResults.openStream()).useDelimiter("[,|\n|,\r]+");
-		while (dataScanner.hasNextLine()) {
-			String vote = dataScanner.nextLine();
-			String[] data = vote.split("[,]");
-			
-			if (!checkID(data[0])) {
-				continue;
-			} else {
-				ArrayList<String> votes = new ArrayList<String>();
-				for (int i = 1; i < data.length; i++) {
-					votes.add(data[i]);
-				}
-				indVotes.add(votes);
-			}
-			
-			
+	public static void storeVote(String[] data) throws IOException {
+		ArrayList<String> votes = new ArrayList<String>();
+		for (int i = 1; i < data.length; i++) {
+			votes.add(data[i]);
 		}
-		
+		indVotes.add(votes);
+
 	}
 	
+	// Method for testing population of votes array
+	public static void printArray() {
+		for (int i = 0; i < indVotes.size(); i++) {
+			int size = indVotes.get(i).size();
+			for (int j = 0; j < size; j++) {
+				System.out.print(indVotes.get(i).get(j) + ", ");
+			}
+			System.out.println();
+		}
+	}
+	
+	// Method for testing population of voterIDs
+	public static void printIDs() {
+		for (int i = 0; i < voterIDs.size(); i++) {
+			System.out.println(voterIDs.get(i));
+		}
+	}
+
 }
---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+		// This is a main method used for testing stubs
+
+		// Test vote collection and ArrayList population of voterIDs and indVotes
+		Votes.Votes();
+	}
+
+}
