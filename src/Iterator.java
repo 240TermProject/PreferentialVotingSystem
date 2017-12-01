f917caa03fd3444c295a9e22653532454b997bad
unknown <hall1kw@cmich.edu>
2017-12-01 04:14:01 -0500

    the iterator utilizes a key/value hashmap to store vote tallies, passes that value to checkMajority and if its greater than 50% of the votes it's passed to setWinner which is private.

--- src/Iterator.java
+++ src/Iterator.java
@@ -1,27 +1,62 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Iterator {
 	
 	//The round that we're on; Do we need one here and in the Round class if they're both protected?
-	protected int roundNum;
+	protected int roundNum = 1;
 	//Candidate with the highest amount of votes
-	protected int highCand;
+	protected String highCand;
 	//Candidate with the lowest amount of votes; Do we need one here and in the Round class if they're both protected?
-	protected int lowCand;
+	protected String lowCand;
+	//Added this to manage candidate names.
+
 	
-	public Iterator(){
-		
+	public Iterator(ArrayList<ArrayList<String>> votes){
+		tallyVotes(votes);
 	}
 	
 	/*
 	 * tallies how many votes each candidate has,
 	 * We will need the ArrayList of votes from the Votes class
 	 */
-	private void tallyVotes(){
+	private static void tallyVotes(ArrayList<ArrayList<String>> votes){
+		int size = votes.size();
+		HashMap<String, Integer> voteTallies = new HashMap<>();
+		for (int i = 0; i < votes.size(); i++) {
+			// This will need to be edited depending on what we pass into Iterator... whether it included voter ID or not
+			for (int j = 1; j < votes.get(i).size(); j++) {
+				voteTallies.put(votes.get(i).get(j), voteTallies.getOrDefault(votes.get(i).get(j), 0) + 1);
+			}
+		}
+		checkMajority(voteTallies, size);
 		
 	}
 	
 	//checks to see if a candidate has more than 50% of the vote
-	private void checkMajority(){
+	private static void checkMajority(HashMap<String, Integer> tallied, int numberOfVotes){
+		Entry<String,Integer> maxEntry = null;
+
+		for(Entry<String,Integer> entry : tallied.entrySet()) {
+		    if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
+		        maxEntry = entry;
+		    }
+		}
+	
+		// Declare winner if the maxEntry has >50% 
+		if (maxEntry.getValue()/numberOfVotes > 0.5) {
+			double percentage = maxEntry.getValue()/numberOfVotes;
+			setWinner(maxEntry.getKey(),percentage);
+		}
+		Entry<String,Integer> minEntry = null;
+		
+		for(Entry<String,Integer> entry : tallied.entrySet()) {
+		    if (minEntry == null || entry.getValue() < minEntry.getValue()) {
+		        minEntry = entry;
+		    }
+		}
 		
 	}
 	
@@ -31,8 +66,10 @@ public class Iterator {
 	}
 	
 	//Sends the winner to the ElectionProcess
-	private int passWinnerUp(){
-		return highCand;
+	private static String setWinner(String name, double percentageOfVote){
+		return (name + " | " + percentageOfVote);
 	}
-	
+	public static String getWinner(String winner) {
+		return winner;
+	}
 }
