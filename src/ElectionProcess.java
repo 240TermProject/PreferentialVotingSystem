d89abe1acc80401c9f753bb2b5161be6307c3262
Javarri <ljavarri@gmail.com>
2017-12-01 21:31:38 -0500

    Implemented singleton design pattern with Documentation

--- src/ElectionProcess.java
+++ src/ElectionProcess.java
@@ -2,41 +2,61 @@ import java.io.File;
 
 public class ElectionProcess {
 	/*
-	 * gets the file that we write after all voters vote through GUI;
-	 * File should contain:
-	 * 	
-	 * 	Every Voter's vote
+	 * gets the file that we write after all voters vote through GUI; File
+	 * should contain:
+	 * 
+	 * Every Voter's vote
 	 */
-	File candidates = new File("candidates.txt"); 
-	
-	//default constructor
-	public ElectionProcess(){
-		
+	File candidates = new File("candidates.txt");
+    //Singleton design pattern
+	private static ElectionProcess oneElection = null;
+	private static boolean electionAvailable = true;
+
+	private ElectionProcess() {
+
 	}
-	
-	//starts the election
-	public void beginElection(){
-		
+
+	public static ElectionProcess getInstance() {
+		//if there hasn't been an election
+		if (electionAvailable) {
+			//double check to see if there's no instantiation
+			if (oneElection == null) {
+				//create an election
+				oneElection = new ElectionProcess();
+			}
+			//an election is available, so set to false
+			electionAvailable = false;
+			//returns the election
+			return oneElection;
+		} else {
+			//if there has been an election, return nothing
+			return null;
+		}
 	}
-	
-	//ends the election
-	public void endElection(){
-		
+
+	// starts the election
+	public void beginElection() {
+
 	}
-	
-	//takes each voter's votes from the textfile
-	public void indVote(){
-		
+
+	// ends the election
+	public void endElection() {
+
 	}
-	
-	//displays the winning candidate
-	public void displayWinner(){
-		
+
+	// takes each voter's votes from the textfile
+	public void indVote() {
+
 	}
-	
-	//assigns candidate their primary key
-	private void assignCandNum(){
-		
+
+	// displays the winning candidate
+	public void displayWinner() {
+
 	}
-	
+
+	// assigns candidate their primary key
+	private void assignCandNum() {
+
+	}
+
 }
